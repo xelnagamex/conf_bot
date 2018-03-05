77f85347446715474d1c71e95e7a01fb13424957
Харченко Михаил <m.kharchenko@tinkoff.ru>
2018-03-05 12:13:09 +0300

    here triggers only on first or last words

--- worker.py
+++ worker.py
@@ -76,7 +76,8 @@ class MessageWorker:
                 self.send(id=conf_id, msg=msg + ' ```')
                 return True
 
-            if msg['message']['text'] == '@here':
+            first, *middle, last = msg.split()
+            if first  == '@here' or last == '@here':
                 conf_id = msg['message']['chat']['id']
                 user_id = msg['message']['from']['id']
                 chat_title = msg['message']['chat']['title']
